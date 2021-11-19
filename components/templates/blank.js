696044b5b5e9516ba5f71e85801bf459031d464a
Lightnet <darkneter@gmail.com>
2021-11-19 12:21:28 -0800

    remove some files.

--- components/templates/blank.js
+++ components/templates/blank.js
@@ -7,17 +7,14 @@
  * Blank Component
  */
 
-//import { useEffect } from 'react';
 //import { getSession } from "next-auth/react";
-//import prisma from './client';
 //import React, {useState, useEffect} from "react";
 
 export default function Component() {
-  /*
-  useEffect(async () => { 
-
-  }, []);
-  */
+  //const [data,setData] = useState
+  //useEffect(() => { 
+  //}, []);
+  
   return (<>
 
   </>);
