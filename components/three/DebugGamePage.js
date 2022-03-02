58e44cf29308f3fcb2bc010a7ed655617b10533c
Lightnet <darkneter@gmail.com>
2022-03-01 18:06:31 -0800

    rework the server name to next and added debug page.

--- components/three/DebugGamePage.js
+++ components/three/DebugGamePage.js
@@ -3,11 +3,39 @@
   Created by: Lightnet
 */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { EntityProvider } from "./context/EntityProvider";
+import DebugPlay from "./game/DebugPlay";
+import {
+  useSearchParams
+} from "react-router-dom";
+import { isEmpty } from "../../lib/helper.mjs";
 
 export default function DebugGamePage({gameid}){
 
+  const [projectID, setProjectID] = useState("");
+  const [searchParams] = useSearchParams();
+
+  useEffect(()=>{
+    //console.log(projectid);
+    if(!isEmpty(gameid)){
+      setProjectID(gameid)
+    }else{
+      console.log("props.gameid NULL!")
+    }
+  },[gameid])
+
+  useEffect(()=>{
+    const currentParams = Object.fromEntries([...searchParams]);
+    //console.log(currentParams);
+    if(currentParams?.gameid){
+      setProjectID(currentParams.gameid)
+    }
+  },[searchParams])
+
   return(<>
-    <label> DebugGamePage </label>
+    <EntityProvider>
+      <DebugPlay gameid={projectID}/>
+    </EntityProvider>
   </>)
 }
